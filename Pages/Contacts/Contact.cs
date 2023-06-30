f9a3d6f2c05994902a818f00ec768df495d90896
maximusfoxy1991 <maximusfoxy1991@gmail.com>
2023-06-30 12:59:35 +0300

    Add project files.

---
+++ Pages/Contacts/Contact.cs
@@ -0,0 +1,23 @@
+﻿namespace ContactManagerWithUsers.Pages.Contacts
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Contact(int id, int userId , string name, string surname, string email, string phone)
+        {
+            Id = id;
+            UserId = userId;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Phone = phone;
+        }
+
+        public Contact() { }
+    }
+}
---
+++ Pages/Contacts/Users.cs
@@ -0,0 +1,49 @@
+﻿namespace ContactManagerWithUsers.Pages.Contacts
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        private List<Contact> contacts = new List<Contact>();
+
+        public Users(int id, string username)
+        {
+            Username = username;
+            Id = id;
+        }
+
+        public Contact GetContact(int id)
+        {
+            for(int i = 0; i < contacts.Count; i++)
+            {
+                if (contacts[i].Id == id)
+                {
+                    return contacts[i];
+                }
+            }
+            return null;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(int id)
+        {
+            for (int i = 0; i < contacts.Count; i++)
+            {
+                if (contacts[i].Id == id)
+                {
+                    contacts.Remove(contacts[i]);
+                }
+            }
+        }
+
+        public void EditContact(int contactId, Contact editedContact)
+        {
+            this.RemoveContact(contactId);
+            contacts.Add(editedContact);
+        }
+    }
+}
