4d0e2d7c116308171ac5816ee37f495ab724bfe4
maximusfoxy1991 <maximusfoxy1991@gmail.com>
2023-07-01 00:44:00 +0300

    Empty files for final pages added

---
+++ Pages/Contacts/CreateContact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactManagerWithUsers.Pages.Contacts
+{
+    public class CreateContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
